88fe5d9b1d0730ebaddb72547faa33f6e434dabd
dreadwing5 <sachinspyk@outlook.com>
2021-08-28 12:49:39 +0530

    Small fix

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require("express");
 const app = express();
 const connection = require("./configs/DBConnection");
 const path = require("path");
-const livereload = require("livereload");
-const connectLiveReload = require("connect-livereload");
 
 const publicDirectory = path.join(__dirname, "public");
 
@@ -12,21 +10,6 @@ const publicDirectory = path.join(__dirname, "public");
 
 app.use(express.static(publicDirectory));
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  console.log("Development Build");
-
-  const liveReloadServer = livereload.createServer();
-
-  liveReloadServer.watch([publicDirectory + "/css", __dirname + "dist"]);
-
-  // ping browser on Express boot, once browser has reconnected and handshaker
-  liveReloadServer.server.once("connection", () => {
-    setTimeout(() => {
-      liveReloadServer.refresh("/");
-    }, 100);
-  });
-  app.use(connectLiveReload());
-}
 app.use("/scripts", express.static(__dirname + "/dist"));
 
 // Set ejs template
@@ -47,6 +30,6 @@ app.use("/", require("./routes/admin"));
 app.use("/", require("./routes/dropdown"));
 
 // Server Running at port 4000
-app.listen("8000", () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log("Server Started ... http://localhost:8000");
 });
