3371bffd3dec76f9c191ba6f7e480715549c9c32
dreadwing5 <sachinspyk@outlook.com>
2021-08-30 14:51:32 +0530

    module fix

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,13 +4,10 @@ module.exports = {
   mode: "production",
   entry: {
     index: [
-      "./public/js/utils/AutoFill.js",
+      "./public/js/controller.js",
       "./public/js/utils/QuillConfig.js",
-      "./public/js/utils/ValidateForm.js",
-      "./public/js/utils/ModalWindow.js",
+      "./public/js/utils/MenuWindow.js",
       "./public/js/utils/Utils.js",
-      "./public/js/utils/Textbox.js",
-      "./public/js/utils/Date.js",
     ],
   },
   output: {
